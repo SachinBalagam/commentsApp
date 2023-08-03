aca5980607f0cc2c33b99d4dc1ce8bbff4ba4095
SachinBalagam <balagamsachin337@gmail.com>
2023-08-03 17:43:03 +0000

    Second Commit

--- src/components/CommentItem/index.js
+++ src/components/CommentItem/index.js
@@ -1,1 +1,30 @@
 // Write your code here
+import './index.css'
+
+const CommentItem = () => (
+  <div>
+    <div>
+      <p className="logo">R</p>
+      <p>Name</p>
+      <p>Time</p>
+    </div>
+    <p>Description is more Powerful</p>
+    <div>
+      <button type="button">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/comments-app/like-img.png"
+          alt="like"
+        />
+        <p>Like</p>
+      </button>
+      <button type="button">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/comments-app/delete-img.png "
+          alt="delete"
+        />
+      </button>
+    </div>
+  </div>
+)
+
+export default CommentItem
--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -1,11 +1,58 @@
-const initialContainerBackgroundClassNames = [
-  'amber',
-  'blue',
-  'orange',
-  'emerald',
-  'teal',
-  'red',
-  'light-blue',
-]
+// const initialContainerBackgroundClassNames = [
+//   'amber',
+//   'blue',
+//   'orange',
+//   'emerald',
+//   'teal',
+//   'red',
+//   'light-blue',
+// ]
 
 // Write your code here
+
+import {Component} from 'react'
+import CommentItem from '../CommentItem'
+import './index.css'
+
+class Comments extends Component {
+  render() {
+    return (
+      <div className="bg-container">
+        <div className="card">
+          <h1>Comments</h1>
+          <div className="image-input-container">
+            <div className="input-container">
+              <p>Say something about 4.0 Technologies</p>
+              <form className="form-container">
+                <input placeholder="Your Name" className="input-name" />
+                <textarea
+                  placeholder="Your Comment"
+                  className="input-comment"
+                />
+                <button type="submit" className="submit-button">
+                  Submit
+                </button>
+              </form>
+            </div>
+            <div className="image-container">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/comments-app/comments-img.png"
+                alt="comments"
+                className="comment-image"
+              />
+            </div>
+          </div>
+          <hr className="horizontal-line" />
+          <p className="comments-count">
+            <span className="count">0</span> Comments
+          </p>
+        </div>
+        <div>
+          <CommentItem />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Comments
